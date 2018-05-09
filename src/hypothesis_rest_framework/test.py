21923b8229993dd17976e0f18d0d8de89e523f9a
LambdaXymox <lambda.xymox@gmail.com>
2018-05-09 05:11:57 +0000

    Subclass TransactionTestCase.

--- src/hypothesis_rest_framework/test.py
+++ src/hypothesis_rest_framework/test.py
@@ -1,8 +1,13 @@
 import hypothesis
 
 import rest_framework.test as rf_test
+hypothesis.extra.django.TestCase
+
+
+class APITransactionTestCase(hypothesis.extra.django.TransactionTestCase):
+    client_class = APIClient
 
 
 class APITestCase(hypothesis.extra.django.TestCase):
-    client_class = rf_test.APIClient
+    client_class = APIClient
 
