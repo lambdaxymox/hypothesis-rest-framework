b882421beda82bf27c3ad824bb43c9f6c5f683d9
LambdaXymox <lambda.xymox@gmail.com>
2019-08-20 00:21:44 -0500

    Update author information.

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ from setuptools.command.test import test as TestCommand
 
 config = dict(
     description = 'Extension of Hypothesis testing framework to integrate property tests into Django Rest Framework.',
-    author = 'Stallmanifold',
-    url = 'https://github.com/stallmanifold/hypothesis-rest-framework',
-    download_url = 'https://github.com/stallmanifold/hypothesis-rest-framework.git',
-    author_email = 'stallmanifold@gmail.com',
+    author = 'LambdaXymox',
+    url = 'https://github.com/lambdaxymox/hypothesis-rest-framework',
+    download_url = 'https://github.com/lambdaxymox/hypothesis-rest-framework.git',
+    author_email = 'lambda.xymox@gmail.com',
     version = '0.1',
     install_requires = ['hypothesis', 'hypothesis[django]', 'djangorestframework'],
     license = "LICENSE-APACHE",
